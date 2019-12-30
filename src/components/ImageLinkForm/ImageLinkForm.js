714dcffa19f6c0acdb4577a7c517a3ebab1dd624
mughees-asif <mughees460@gmail.com>
2019-12-30 20:09:52 +0000

    deployment

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p className='ph5 f3 center white'>
+        {<b>Copy and paste an image URL from any search engine. 
+          The more faces you scan, the higher your count!</b>}
+      </p>
+      <div className='center'>
+        <div className='form center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            onClick={onButtonSubmit}
+          >Detect</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageLinkForm;
---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    }
+  }
+
+  onEmailChange = (event) => {
+    this.setState({signInEmail: event.target.value})
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+    fetch('https://peaceful-shelf-20406.herokuapp.com/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
+        }
+      })
+  }
+
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-2 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw9 ph0 white mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy white f6" htmlFor="email-address">Email</label>
+                <input
+                  className="pa2 input-reset ba white bg-transparent hover-bg-white hover-black w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy white f6" htmlFor="password">Password</label>
+                <input
+                  className="b pa2 input-reset ba white bg-transparent hover-bg-white hover-black w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--white bg-white grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
+              />
+            </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
+}
+
+export default Signin;
